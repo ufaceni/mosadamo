bf08a864c3e5b6edc4e8c2ac3a0a377bf4975f89
ufaceni <pepa.armin@gmail.com>
2017-01-21 19:38:10 +0100

    source code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var modalHolder = document.getElementById('myModal');
+
+function modal (id){
+    modalHolder.style.display = "block";
+    var modalImg = document.getElementById("imageModal");
+    var captionText = document.getElementById("caption");
+    var element = document.getElementById(id);
+    modalImg.src = element.src;
+    captionText.innerHTML = element.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modalHolder.style.display = "none";
+}
