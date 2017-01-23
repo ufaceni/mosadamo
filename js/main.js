1d79b790637de23d9d9bfb1f49aafa44f296cbe1
ufaceni <pepa.armin@gmail.com>
2017-01-23 21:47:39 +0100

    fixed header name and apparence

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modalHolder.style.display = "none";
 }
+
+function removeWebHostStuff() {
+    var url = location.href;
+    if (url.indexOf('file:///') === -1 && url.indexOf('localhost') === -1) {
+        var divs = document.body.getElementsByTagName('div');
+        var webHostDiv = divs[divs.length-1];
+        webHostDiv.parentNode.removeChild(webHostDiv);
+    }
+}
+
+window.onload = function() {
+   // setTimeout(removeWebHostStuff, 1000);
+   removeWebHostStuff();
+};
+
